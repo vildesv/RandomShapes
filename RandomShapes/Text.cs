9b772f42fe2b54ad2c499b05718b7a4d45ec2609
vildesv <vilde@getacademy.no>
2022-01-14 14:47:46 +0100

    Add project files.

---
+++ RandomShapes/Text.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomShapes
+{
+    class Text : Shape
+    {
+        private string _text;
+
+        public Text(int x, int y, string text, Random random) : base(random)
+        {
+            Y = y;
+            X = x;
+            _text = text;
+        }
+
+        public override string GetCharacter(int row, int col)
+        {
+            if (row != Y || col < X || col >= X + _text.Length) return null;
+            var index = col - X;
+            return _text[index].ToString();
+        }
+    }
+}
